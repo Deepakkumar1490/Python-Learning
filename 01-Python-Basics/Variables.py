bf079dec9b2f0f5a0ff5f3951ca5fa337ed6a39e
Deepakkumar1490 <rdeepakkumar1490@gmail.com>
2026-08-11 09:18:20 +0530

    Added Variables,Integer, Float, String

---
+++ 01-Python-Basics/Variables.py
@@ -0,0 +1,23 @@
+#What is a variable?	A named container that stores a value.
+#Why use variables?	To store and reuse data, avoid repetition, and make code easier to maintain.
+#When should you use them?	Whenever you need to remember information for later use in your program.
+#How do you create one?	variable_name = value
+#Can a variable's value change?	Yes, you can assign a new value at any time.
+#Does Python require declaring a type?	No. Python automatically determines the type (dynamic typing).
+
+
+#single value assignment
+a = 10
+print(a)
+
+#multiple vale assignment
+x,y,z = 10, 20, 30
+print(x)
+print(y)
+print(z)
+
+#single value to multiple variables
+a=b=c=100
+print(a)
+print(b)
+print(c)
